17bda1e6f2596c0af55f92c20c4367e56eb26275
jojo-tey <dosagam6301@gmail.com>
2021-03-04 17:50:53 +0100

    FE templates

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.tabs').tabs();
+    $('.materialboxed').materialbox();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
