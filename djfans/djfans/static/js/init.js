8ef457aa33e3a2a44e6602b58be3ee2720cd9f13
jojo-tey <dosagam6301@gmail.com>
2021-03-29 18:47:11 +0200

    css dark

--- djfans/djfans/static/js/init.js
+++ djfans/djfans/static/js/init.js
@@ -1,26 +1,26 @@
 (function ($) {
-    $(function () {
+  $(function () {
 
-        $('.sidenav').sidenav();
-        $('.tabs').tabs();
-        $('.materialboxed').materialbox();
-        $('select').formSelect();
-        $('.slider').slider();
-        $('.tooltipped').tooltip();
-        $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.tabs').tabs();
+    $('.materialboxed').materialbox();
+    $('select').formSelect();
+    $('.slider').slider();
+    $('.tooltipped').tooltip();
+    $('.modal').modal();
 
-        $("addtolistform").submit(function () {
-            $.ajax({
-                url: '/profile/addtolist',
-                data: $("#addtolistform").serialize(),
-                cache: false,
-                type: "post",
-                success: function (data) {
-                    console.log("Person added succesfully")
-                }
-            });
-            return false;
-        });
+    $("addtolistform").submit(function () {
+      $.ajax({
+        url: '/profile/addtolist',
+        data: $("#addtolistform").serialize(),
+        cache: false,
+        type: "post",
+        success: function (data) {
+          console.log("Person added succesfully")
+        }
+      });
+      return false;
+    });
 
-    }); // end of document ready
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
