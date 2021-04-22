17dc10a59f08df7c0f353baf5ca31f90d3afa4c1
jojo-tey <dosagam6301@gmail.com>
2021-04-22 19:42:12 +0200

    update

--- djfans/djfans/settings.py
+++ djfans/djfans/settings.py
@@ -132,10 +132,10 @@ WSGI_APPLICATION = 'djfans.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'djfansdb2.cfyleonctk8z.eu-central-1.rds.amazonaws.com',
+        'HOST': os.environ['DB_HOST'],
         'PORT': '5432',
-        'NAME': 'djfansdb',
-        'USER': 'djfansdbmaster',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD']
     }
 }
