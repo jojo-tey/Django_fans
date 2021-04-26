e824f59b6bae73946eecffa911919babb9f9157e
jojo-tey <dosagam6301@gmail.com>
2021-04-26 02:43:56 +0200

    admin

--- djfans/authy/initadmin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-
+python manage.py createsuperuser --noinput
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
