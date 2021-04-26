38ddd190766c36e7559d5a29fafed42ae6cd0b80
jojo-tey <dosagam6301@gmail.com>
2021-04-26 15:30:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,9 @@ import os
 python manage.py makemigrations
 python manage.py migrate --noinput
 
-python manage.py createcachetable
-
-if ['$DJANGO_SUPERUSER_USERNAME']
-then
-    python manage.py createsuperuser \
-        --noinput \
-        --username ['$DJANGO_SUPERUSER_USERNAME'] \
-        --email ['$DJANGO_SUPERUSER_USERNAME']
-fi
-
-
-
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=testpass \
+python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 
 gunicorn djfans.wsgi:application --bind 0.0.0.0:8000
