1483633826e039ce140d0d1351868b269c8f058a
jojo-tey <dosagam6301@gmail.com>
2021-04-26 20:17:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@
 
 python manage.py makemigrations
 python manage.py migrate --noinput
-python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 
 gunicorn djfans.wsgi:application --bind 0.0.0.0:8000
