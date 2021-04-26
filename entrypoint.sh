6de31db9f624087dfbb06e1fbe68157265fb9e94
jojo-tey <dosagam6301@gmail.com>
2021-04-26 03:03:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-python manage.py createsuperuser --noinput
+DJANGO_SUPERUSER_PASSWORD=testpass 
+python manage.py createsuperuser --username admin --email admin@email.com --noinput
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
