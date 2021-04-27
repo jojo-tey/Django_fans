3142ac68b50766bdb345eb2ac20044fd9fdc71eb
jojo-tey <dosagam6301@gmail.com>
2021-04-27 21:51:02 +0200

    test

--- djfans/djfans/settings.py
+++ djfans/djfans/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     # provider
     'allauth.socialaccount.providers.github',
-    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
