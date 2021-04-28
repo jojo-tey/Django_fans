8cf7ee6d19d73234474b7da498cad49e20306708
jojo-tey <dosagam6301@gmail.com>
2021-04-28 17:17:21 +0200

    Settings devide

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+# Djfans
+
+- Personal project for Onlyfans clone
+
+### Used skill
+- Django
+- Docker-compose
+- Terraform 
+- Jenkins
+
+
+
+
+
+
 - 이슈와 해결방법
 
 포스팅이 멀티아규먼트를 뱉어 에러 발생
---
+++ djfans/djfans/settings/dev.py
@@ -0,0 +1,34 @@
+from .base import *
+
+
+# Running env - Local
+
+# key for test = '-nro!j**h$!c=d(*r30+u46g$^(oge1!*9ymb4v8ks9%mwyim%'
+SECRET_KEY = 'Paste the test key'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+
+# Database - Sqlite3
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# static setting
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'djfans/static'),
+]
+
+
+# media setting
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
