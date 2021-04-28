9b4322b44745ec23921551093690ada135233da0
jojo-tey <dosagam6301@gmail.com>
2021-04-28 18:34:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 
 
 python manage.py makemigrations --settings=djfans.prod
-python manage.py migrate --settings=djfans.prod
-python manage.py collectstatic --settings=djfans.prod
+python manage.py migrate --settings=djfans.prod --noinput
+python manage.py collectstatic --settings=djfans.prod --noinput
 
 gunicorn djfans.wsgi:application --bind 0.0.0.0:8000
